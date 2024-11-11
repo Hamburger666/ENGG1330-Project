d79342b2d820a85c7bc2d7c205bf95317e5354ff
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2024-11-11 20:28:02 +0800

    Update menus.py

--- menus.py
+++ menus.py
@@ -2,19 +2,23 @@ import curses
 
 
 class Menus:
+    #draws a top-down menu (or any text from top-down)
     @staticmethod
-    def draw_menu(screen, menu_elements, highlighted, *, brackets = False, y_shift = 0, x_shift = 0):
-        Menus.draw_title(screen)
+    def draw_menu(screen, menu_elements, highlighted = (), *, brackets = False):
+        Menus.draw_logo(screen)
         for i in range(len(menu_elements)):
-            element = f"< {menu_elements[i]} >" if i == highlighted[0] else (f" <{menu_elements[i]}> " if brackets else f"  {menu_elements[i]}  ")
+            element = f" <{menu_elements[i]}> " if brackets else f"  {menu_elements[i]}  "
+            if i == highlighted[0]:
+                element = f"< {menu_elements[i]} >"
             screen.addstr(  max((screen.getmaxyx()[0] - len(menu_elements))//2, 11) + i,
-                            (screen.getmaxyx()[1] - len(element))//2,
-                            element, curses.color_pair(7) if i == highlighted[0] else curses.color_pair(6) | curses.A_BOLD) 
+                            (screen.getmaxyx()[1] - len(element))//2, element,
+                            curses.color_pair(7) if i == highlighted[0] else curses.color_pair(6) | curses.A_BOLD) 
         screen.refresh()
 
+    #draws logo with 2 bars on top and bottom
     @staticmethod
-    def draw_title(screen):
-        title = [
+    def draw_logo(screen):
+        logo = [
             " _______ _    _ ______        _               ____  ",
             "|__   __| |  | |  ____|      | |        /\\   |  _ \\ ",
             "   | |  | |__| | |__         | |       /  \\  | |_) |",
@@ -22,15 +26,15 @@ class Menus:
             "   | |  | |  | | |____       | |____ / ____ \\| |_) |",
             "   |_|  |_|  |_|______|      |______/_/    \\_\\____/ "]
 
+        #draws the bars and logo in top-down order
         screen.addstr(0, 0, "=" * (screen.getmaxyx()[1] - 1), curses.color_pair(6))
-        for i in range(len(title)):              
-            screen.addstr(2+i, (screen.getmaxyx()[1]-len(title[i]))//2, title[i], curses.color_pair(6) | curses.A_BOLD)
-        
+        for i in range(len(logo)):              
+            screen.addstr(  2+i, (screen.getmaxyx()[1]-len(logo[i]))//2, 
+                            logo[i], curses.color_pair(6) | curses.A_BOLD)
         screen.addstr(9, 0, "=" * (screen.getmaxyx()[1] - 1), curses.color_pair(6))
 
     @staticmethod
     def draw_main_menu(screen, highlighted):
-
         options = [
             "New Game",
             "Continue",
@@ -39,29 +43,35 @@ class Menus:
         ]
         Menus.draw_menu(screen, options, highlighted, brackets = True)
 
-
-
     @staticmethod
     def draw_help_menu(screen):
-
         text = [
             "=====  HOW TO PLAY  =====",
             "",
             "Choose circuit parts with WASD or Arrow keys",
             "Press ENTER or SPACE to select a part",
             "Move the selected part with WASD or Arrow Keys",
-            "Parts in their correct positions will appear blue",
-            "Reconstruct a complete path to complete the level",
+            "Parts with the correct shape in a position will appear blue",
+            "Red crosses will apear on parts at incorrect positions",
+            "Reconstruct a complete circuit to complete the level",
+            "However, if any circuit part is in the wrong position, the circuit explodes.",
+            "You can press P or ESC while in game to pause"
             "",
-            "",
-            "NOTE: The game does NOT have autosave"
             "",
             "Press any key to go back"
-
         ]
         Menus.draw_menu(screen, text, (-1, -1))
-        
 
+    @staticmethod
+    def draw_story_menu(screen):
+        text = [
+            "===== BACKGROUND STORY =====",
+            "",
+            "You are a researcher working at an advanced materials lab.",
+            "Just a week ago,",
+            "And you are "
+        ]
+        
     @staticmethod
     def draw_pause_menu(screen, highlighted):
         options = [
@@ -72,22 +82,28 @@ class Menus:
         ]
         text = "=====  PAUSED  ====="
         Menus.draw_menu(screen, options, highlighted, brackets = True)
-        screen.addstr(screen.getmaxyx()[0]//2-4, (screen.getmaxyx()[1] - len(text))//2, text, curses.color_pair(6) | curses.A_BOLD)
+        screen.addstr(  screen.getmaxyx()[0]//2-4, (screen.getmaxyx()[1] - len(text))//2, 
+                        text, curses.color_pair(6) | curses.A_BOLD)
         screen.refresh()
 
-
+    @staticmethod
+    def draw_game_over_menu(screen, highlighted):
+        options = [
+            "Restart",
+            "Quit"
+        ]
+        Menus.draw_menu(screen, options, highlighted, True)
 
     @staticmethod
     def draw_error_menu(screen, error_msg):
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
         screen.timeout(-1)
         screen.clear()
-        screen.addstr(" ")
-        screen.refresh()
-        screen.clear()
         text = "Press any key to exit..."
-        screen.addstr(screen.getmaxyx()[0]//2 - 1, (screen.getmaxyx()[1] - len(error_msg))//2, error_msg, curses.color_pair(1) | curses.A_BOLD)
-        screen.addstr(screen.getmaxyx()[0]//2 + 1, (screen.getmaxyx()[1]-len(text))//2, text, curses.color_pair(1) | curses.A_BOLD)
+        screen.addstr(  screen.getmaxyx()[0]//2 - 1, (screen.getmaxyx()[1] - len(error_msg))//2, 
+                        error_msg, curses.color_pair(1) | curses.A_BOLD)
+        screen.addstr(  screen.getmaxyx()[0]//2 + 1, (screen.getmaxyx()[1]-len(text))//2, 
+                        text, curses.color_pair(1) | curses.A_BOLD)
         screen.refresh()
         screen.getch()
 
