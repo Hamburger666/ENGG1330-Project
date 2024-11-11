5be0ffe75e7aec4be88a2bf03501f73e94a227c3
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2024-11-11 20:28:11 +0800

    Update tile.py

--- tile.py
+++ tile.py
@@ -2,9 +2,9 @@ from enums import TileTypes
 import curses
 
 class Tile:
-    #constructor
-    def __init__(self, tile_type):
+    def __init__(self, tile_type, number):
         self.tile_type = tile_type
+        self.number = number
 
     #overrides default == sign comparison
     def __eq__(self, other):
@@ -12,18 +12,20 @@ class Tile:
             return self.tile_type == other.tile_type
         return False
 
-    #draws the tile
-    def draw(self, screen, row, col, is_correct, is_highlighted, is_selected, size, blink):
+    #displays the tile at the given position
+    def draw(self, screen, row, col, is_correct_pos, is_correct_order, is_highlighted, is_selected, size, blink):
         color = [curses.color_pair(1), curses.color_pair(2)]
         if is_highlighted and not blink:
             color[1] = curses.color_pair(3)
             if is_selected:
                 color[1] = curses.color_pair(4) 
-        elif is_correct:
-            color[1] = curses.color_pair(5)
+        elif is_correct_pos:
+            color[1] = curses.color_pair(8) | curses.A_BOLD
+            if is_correct_order:
+                color[1] = curses.color_pair(5)
         for i in range(3):
             for j in range(3):
+                megapixel = "++" if i == 1 and j == 1 else "  " 
                 screen.addstr(  (screen.getmaxyx()[0] - size * 3)//2 + row * 3 + i, 
                                 (screen.getmaxyx()[1] - size * 6)//2 + (col * 3 + j) * 2, 
-                                "__", color[self.tile_type.value[i][j]])
-
+                                megapixel, color[self.tile_type.value[i][j]])
