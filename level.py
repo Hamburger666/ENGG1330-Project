47f1885dbd2bc3ee8662628b83f6ea8280b4f471
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2024-11-12 10:39:46 +0800

    Update level.py
    fixed edge cases for the generation algorithm (given an N*N grid where N is an odd number, there exists no Hamiltonian path for any (y, x) where y is odd and x is even, vice versa)

--- level.py
+++ level.py
@@ -10,13 +10,22 @@ class Level:
         #initialize grid to be N*N unexplored nodes and path to be empty array
         self.grid = [[0 for _ in range(size)] for _ in range (size)] 
         self.path = []
-        self.generate_path( random.randint(0, size-1), 
-                            random.randint(0, size-1))
-
+        #generates the circuit using a wrapper function
+        self.generate_path_safe(size)
         self.generate_tile_map()
         self.reference_map = self.tile_map.copy()
         self.randomize_tile_map()
-            
+    
+    #wrapper for path generation, solves edge cases
+    #note: 
+    def generate_path_safe(self, n):
+        start_y, start_x = random.randint(0, n-1), random.randint(0, n-1)
+        if n % 2 == 1:
+            start_x = start_y
+        self.generate_path(start_y, start_x)
+
+    #recursive function to generate a Hamiltonian path
+    #Warning: time complexity is O(n^2)
     def generate_path(self, row, col) -> bool:
         #base case: path covered the whole map
         if len(self.path) == self.size ** 2:
@@ -26,14 +35,14 @@ class Level:
         moves = list(Directions)
         random.shuffle(moves)
 
+        #check if the current node is explored or out of bounds
+        if self.is_out_of_bounds(row, col):
+            return False
+        elif self.grid[row][col] == 1:
+            return False    
+
         #try all directions to see which leads to an unexplored node
         for move in moves:
-            #check if the current node is explored or out of bounds
-            if self.is_out_of_bounds(row, col):
-                return False
-            elif self.grid[row][col]:
-                return False    
-        
             #set current node to traversed and add it to path to record
             self.grid[row][col] = 1
             self.path.append((row, col))
@@ -59,8 +68,10 @@ class Level:
             node = self.path[i]
             new_tile = None
             if i == 0:
+                #start tile
                 new_tile = Tile(TileTypes.get_tile_type(node, self.path[i + 1]), i + 1)
             elif i == len(self.path) - 1:
+                #end tile
                 new_tile = Tile(TileTypes.get_tile_type(node, self.path[i - 1]), i + 1)
             else:
                 new_tile = Tile(TileTypes.get_tile_type(node, self.path[i + 1], self.path[i - 1]), i + 1)
@@ -78,6 +89,7 @@ class Level:
         self.tile_map[pos1[0]][pos1[1]], self.tile_map[pos2[0]][pos2[1]] = self.tile_map[pos2[0]][pos2[1]], self.tile_map[pos1[0]][pos1[1]]
 
     #check if the level is completed, and if tiles are in correct order
+    #returns a tuple of (bool, bool)
     def is_complete(self):
         total_correct, is_order_correct = 0, True
         for row in range(self.size):
@@ -98,3 +110,42 @@ class Level:
                 self.tile_map[row][col].draw(   screen, row, col, is_correct_pos, is_correct_order, 
                                                 is_highlighted, is_selected, self.size, blink)
         screen.refresh()
+
+    #DEBUG draws the level after & before shuffle, also displays path
+    def debug(self, screen):
+
+        screen.clear()
+        for i in range(len(self.path)):
+            screen.addstr(i, 0, str(self.path[i]))
+        screen.refresh()
+        screen.getch()
+        screen.refresh()
+        for row in range(self.size):
+            for col in range(self.size):
+                self.tile_map[row][col].draw(screen, row, col, False, False, False, False, self.size, False)
+        screen.refresh()
+        screen.getch()
+        screen.clear()
+        for row in range(self.size):
+            for col in range(self.size):
+                self.reference_map[row][col].draw(screen, row, col, False, False, False, False, self.size, False)
+        screen.refresh()
+        screen.getch()
+
+
+#DEBUG driver code
+def main(scr):
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)     #background
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)     #incorrect position & order
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_WHITE)     #highlighted
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_YELLOW)   #selected
+    curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLUE)       #correct position & order
+    curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)     #default text 
+    curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)     #highlighted text
+    curses.init_pair(8, curses.COLOR_RED, curses.COLOR_BLUE)        #correct position, wrong order
+
+    test = Level(5)
+    test.debug(scr)
+
+curses.wrapper(main)
+
