cfa94f1e0d4a17d81e8e49a5c5633de640da5f78
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2024-11-12 10:50:46 +0800

    Update tile.py
    added more comments and added idiot redirection

--- tile.py
+++ tile.py
@@ -15,17 +15,26 @@ class Tile:
     #displays the tile at the given position
     def draw(self, screen, row, col, is_correct_pos, is_correct_order, is_highlighted, is_selected, size, blink):
         color = [curses.color_pair(1), curses.color_pair(2)]
+        #positive blink -> display "background"
+        #thus negative blink displays foreground
         if is_highlighted and not blink:
             color[1] = curses.color_pair(3)
             if is_selected:
                 color[1] = curses.color_pair(4) 
         elif is_correct_pos:
+            #correct pos, incorrect order
             color[1] = curses.color_pair(8) | curses.A_BOLD
             if is_correct_order:
+                #correct pos & order
                 color[1] = curses.color_pair(5)
         for i in range(3):
             for j in range(3):
-                megapixel = "++" if i == 1 and j == 1 else "  " 
+                megapixel = "++" if i == 1 and j == 1 else "  " #only display the scrach in the center
                 screen.addstr(  (screen.getmaxyx()[0] - size * 3)//2 + row * 3 + i, 
                                 (screen.getmaxyx()[1] - size * 6)//2 + (col * 3 + j) * 2, 
                                 megapixel, color[self.tile_type.value[i][j]])
+
+
+#tells players how to correctly launch the game should they try to open this file instead
+if __name__ == "__main__":
+    print("Please launch the game with the command 'python game.py'.")
